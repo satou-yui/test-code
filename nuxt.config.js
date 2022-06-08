2094f43e99ca80286ae76060132fed51ff8b0394
satou_yui <yui_satou@commude.co.jp>
2022-06-08 15:27:07 +0900

    追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,9 +29,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/dateFilter',
     '~/plugins/descriptionFilter' // 追加
   ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
